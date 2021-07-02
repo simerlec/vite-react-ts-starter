c158f9c45196c30eb61d6ffe0717d6389cf61619
Christopher Simerle <christopher@getmimo.com>
2021-07-02 09:44:03 +0200

    batch update deps, add test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,9 @@ test("renders learn react link", () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("does not render an learn angular link", () => {
+  const { queryByText } = render(<App />);
+  const linkElement = queryByText(/learn angular/i);
+  expect(linkElement).not.toBeInTheDocument();
+});
