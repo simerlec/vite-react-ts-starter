744d932e93138bfb8f6906d9498bca958e2151a0
Christopher Simerle <simerle.c@gmail.com>
2022-01-09 17:04:25 +0100

    update deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,12 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: "jit",
-  darkMode: "class", // or 'media' or 'class'
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       animation: {
         "spin-slow": "spin 20s linear infinite",
-      },
-      colors: {
-        teal: colors.teal,
-        yellow: colors.yellow,
       },
     },
   },
