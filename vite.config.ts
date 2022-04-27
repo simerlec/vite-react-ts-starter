259f183c7dbb9715a9619056a5b3cdefe7c5664f
Christopher Simerle <simerle.c@gmail.com>
2022-04-27 07:24:21 +0000

    Update 3 files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import codesandbox from "@gsimone/codesandbox-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), codesandbox()],
 });
