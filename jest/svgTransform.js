70eb9285b9064e0f70c194d5fb64c46795f0632d
Christopher Simerle <simerle.c@gmail.com>
2022-07-04 17:12:12 +0200

    update deps, fix transformer

--- jest/svgTransform.js
+++ jest/svgTransform.js
@@ -1,9 +1,9 @@
 module.exports = {
   process() {
-    return 'module.exports = {};';
+    return { code: "" };
   },
   getCacheKey() {
     // The output is always the same.
-    return 'svgTransform';
+    return "svgTransform";
   },
 };
