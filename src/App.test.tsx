7a9b1b3776eeaf1bbac886cbc3afb2d1c5d8d261
Christopher Simerle <simerle.c@gmail.com>
2023-11-13 21:17:30 +0100

    update deps

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
-import { expect, test } from "vitest";
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
